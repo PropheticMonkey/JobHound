56c11c705cbec71dc682e18fab1029eda66bd2d6
PropheticMonkey <cliftonlboyd@gmail.com>
2016-01-10 12:39:03 -0500

    Initial commit

---
+++ WebFetcher.py
@@ -0,0 +1,73 @@
+import urllib
+
+
+
+def getSite(url):
+    #input url
+    #output .txt of site code
+    connection = urllib.urlopen(url)
+    siteText = connection.read()
+    #print(siteText)
+    connection.close()
+    return siteText
+
+def getTxt(txt):
+    txtFile = open(txt)
+    contents_of_file = txtFile.read()
+    #print(contents_of_file)
+    #print("...........")
+    txtFile.close()
+    return contents_of_file
+
+def getLinks(txt):
+    #needs to be modified to grab legitimate links instead of
+    #anything inside <a href=" and </a>
+    
+    #input string of the contents of a website
+    #output array of the urls of all links within the website
+    links = []
+    isLink = False
+    newLink = ""
+    for i,e in enumerate(txt):
+        if isLink == True:
+            if e == "<" and txt[i:i+4] == "</a>":
+                #print("</a> test True")
+                isLink = False
+                print(newLink)
+                links += [newLink]
+                newLink = ""
+            else:
+                newLink = newLink+e
+        else:
+            if e == '"' and txt[i-8:i] == "<a href=":
+                #print("<a href= test True")
+                isLink = True
+            else:
+                pass 
+    print("links...................")
+    print(links)
+    return links
+
+def removeHTML(txt):
+    #input string of website source code
+    #output string with HTML tags removed
+    cleanedText = ""
+    html = False
+    #print(text)
+    #loop removing html tags
+    for e in (txt):
+        if html == False:
+            if e == "<":
+                html = True
+            else:
+                cleanedText = cleanedText+e        
+        else:
+            if e == ">":
+                html = False
+                
+    print(cleanedText)
+    return cleanedText
+
+
+getLinks(getTxt(r"C:\Python27\testfiles\testhtml.txt"))
+    
